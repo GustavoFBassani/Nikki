7c91d5de3f048410de5ec5e3570647de16eaeccf
Rafael Toneto <rafaeltoneto@Rafaels-MacBook-Pro-2.local>
2025-12-05 16:57:15 -0300

    CreditsView created

---
+++ Nikki/Features/Canvas/Views/CreditsView.swift
@@ -0,0 +1,87 @@
+//
+//  CreditsView.swift
+//  Nikki
+//
+//  Created by Rafael Toneto on 05/12/25.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        
+        VStack{
+            HStack{
+//                Button {
+//                    
+//                }
+                
+                Text("Credits")
+                    .font(.custom("CaveatBrush-Regular", size: 24))
+            }
+            
+            VStack(spacing: 24){
+                VStack(spacing: 8){
+                    Text("Tree")
+                        .font(.custom("CaveatBrush-Regular", size: 22))
+                        .frame(width: 361, alignment: .leading)
+
+                    Text("3D Model: “Cherry Tree” Created by: local.yany Available in: https://sketchfab.com/3d-models/cherry-tree-5f6d9059b971429999343e0f398bf487 License: Creative Commons Attribution 4.0 (CC BY 4.0) https://creativecommons.org/licenses/by/4.0/")
+                        .font(.system(size: 13, weight: .regular))
+                        .frame(width: 361, alignment: .leading)
+                }
+                .padding(.horizontal)
+
+                VStack(spacing: 8){
+                    Text("Scenery")
+                        .font(.custom("CaveatBrush-Regular", size: 22))
+                        .frame(width: 361, alignment: .leading)
+
+                    Text("3D Model “Japanese garden” — created by Katydid. License: Sketchfab Free Standard (https://sketchfab.com/licenses/free-standard). Modified by Martina Adegas. Available in: https://sketchfab.com/3d-models/japanese-garden-3083a8ae73bd47c68e655713915787f5")
+                        .font(.system(size: 13, weight: .regular))
+                        .frame(width: 361, alignment: .leading)
+                }
+                .padding(.horizontal)
+
+                VStack(spacing: 8){
+                    Text("Gate")
+                        .font(.custom("CaveatBrush-Regular", size: 22))
+                        .frame(width: 361, alignment: .leading)
+
+                    Text("Model: Japanese Torii Gate Author: Sahir Virmani Model's Link: https://sketchfab.com/3d-models/japanese-torii-gate-2027a248de1b4b70985ff97e708fb50d License: Creative Commons Attribution (CC BY 4.0)")
+                        .font(.system(size: 13, weight: .regular))
+                        .frame(width: 361, alignment: .leading)
+                }
+                .padding(.horizontal)
+
+                VStack(spacing: 8){
+                    Text("Bandstand")
+                        .font(.custom("CaveatBrush-Regular", size: 22))
+                        .frame(width: 361, alignment: .leading)
+
+                    Text("Model: Japan HW Author: Kumekovaaa Model's Link: https://sketchfab.com/3d-models/japan-hw-c3d60d0e25224ea592e87425a2f3b130 License: CC BY 4.0 — Creative Commons Attribution Link da licença: https://creativecommons.org/licenses/by/4.0/")
+                        .font(.system(size: 13, weight: .regular))
+                        .frame(width: 361, alignment: .leading)
+                }
+                .padding(.horizontal)
+
+                VStack(spacing: 8){
+                    Text("Lamp")
+                        .font(.custom("CaveatBrush-Regular", size: 22))
+                        .frame(width: 361, alignment: .leading)
+
+                    Text("Model: Chinese Lamp - 4096px² Author: Mark Peters Model's Link: https://sketchfab.com/3d-models/chinese-lamp-4096px2-9eddb4de98274410b7405f7ea6667f3a License: Creative Commons Attribution (CC BY)")
+                        .font(.system(size: 13, weight: .regular))
+                        .frame(width: 361, alignment: .leading)
+                }
+                .padding(.horizontal)
+            }
+
+        }
+        
+    }
+}
+
+#Preview {
+    CreditsView()
+}
