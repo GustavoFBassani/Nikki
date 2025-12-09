1f6cf5610f176dc53423eaa814cdcee033706b00
Alex Fraga <144173950+alex-fraga@users.noreply.github.com>
2025-12-09 17:41:33 -0300

    removed useless lines

--- Nikki/Managers/EditorData.swift
+++ Nikki/Managers/EditorData.swift
@@ -50,9 +50,6 @@ class EditorData {
         // featureSet.remove(.loupes)
         // let controller = PaperMarkupViewController(supportedFeatureSet: featureSet) caso queira escolher as ferramentas igual WWDC
         
-        controller.overrideUserInterfaceStyle = .light
-        controller.view.overrideUserInterfaceStyle = .light
-        
         // Caso usemos delegate tem que descomentar aqui
         // controller.delegate = self
         controller.loadViewIfNeeded()
